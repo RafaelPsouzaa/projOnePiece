7db5643dc62c3728c8136f49d969b0014ed8fe04
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-10-02 15:51:32 -0300

    javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-document.querySelectorAll('.botao');
+const botoes = document.querySelectorAll('.botao');
+
+botoes.forEach(botao => {
+    botao.addEventListener("click",() => {
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        console.log(botaoSelecionado);
+        botaoSelecionado.classList.remove("selecionado");
+        
+        botao.classList.add("selecionado");
+    });
+
+});
+
+//https://www.youtube.com/watch?v=hzw1m15CGAo&t=3221s&ab_channel=DevemDobro 1:11:16
