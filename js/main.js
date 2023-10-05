1b3095030494dfb92ff3bb694d77102554f367dd
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2023-10-05 17:03:32 -0300

    button corrrection

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 const botoes = document.querySelectorAll('.botao');
 const personagens = document.querySelectorAll('.personagem');
-botoes.forEach((botao,indice) => {
+botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
-        const botaoSelecionado = document.querySelector(".botao.selecionado");
-        botaoSelecionado.classList.remove("selecionado");
+        desselecionar();
+        desselecionarPersonagem();
         botao.classList.add("selecionado");
-        const personagemSelecionado = document.querySelector(".personagem.selecionado");
-        personagemSelecionado.classList.remove("selecionado");
         personagens[indice].classList.add("selecionado");
     });
 
 });
 
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionar() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
 //https://www.youtube.com/watch?v=hzw1m15CGAo&t=3221s&ab_channel=DevemDobro 1:11:16
